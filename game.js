68f8b2658d22fb3c4cd073b9cdaa32bd7f9e87d8
Drew091 <hoppal.andras91@gmail.com>
2023-03-18 14:18:25 +0100

    Add button to change grid size, set max value to 100

--- game.js
+++ game.js
@@ -4,25 +4,36 @@ function colorBlack() {
 
 const container = document.getElementById("container");
 
-function makeColumn (grid) {
-  for (let j=0; j<grid * grid; j++){
-    let cells = document.createElement("div");
-    let squareWidth = 500/grid;
-    let squareHeight = squareWidth;
-    cells.style.width = squareWidth +"px";
-    cells.style.height = squareHeight +"px";
-    cells.setAttribute("id","cell");
-    cells.addEventListener("mouseover", colorBlack);
-    container.appendChild(cells)
+function changeGrid () {
+  let grid = prompt("Please enter the new grid size. (Max 100)", "16");
+
+  if(grid != null && grid <=100) {
+    const cellClasses = document.querySelectorAll('.cell');
+
+    cellClasses.forEach(cell => {
+      cell.remove();
+    });
+  }
+  makeGrid (grid)
+}
+
+function makeGrid (grid) {
+
+  if (grid <= 100) {
+    for (let i=0; i<grid * grid; i++){
+      let cells = document.createElement("div");
+      let squareWidth = 500/grid;
+      let squareHeight = squareWidth;
+
+      cells.style.width = squareWidth +"px";
+      cells.style.height = squareHeight +"px";
+      cells.classList.add("cell");
+      cells.addEventListener("mouseover", colorBlack);
+      container.appendChild(cells)
+    }
+  }else{
+    alert("100x100 is the maximum grid size");
   }
 }
-makeColumn (40);
 
-
-//https://dirask.com/posts/JavaScript-dynamically-create-grid-of-divs-using-flexbox-139abp
-//https://stackoverflow.com/questions/68563516/add-event-listener-to-multiple-divs-with-same-class-in-a-container
-
-//document.getElementById("square").addEventListener("mouseover", function() {
-//  document.getElementById("square").classList.add("black");
-//  document.getElementById("square").style.backgroundColor = "black";
-//});
+makeGrid (16);
